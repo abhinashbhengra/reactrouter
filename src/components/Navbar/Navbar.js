253546e09c8b870aae5dc9a9b0c5969fc2a45677
Abhinash Bhengra <abhinashbhengra170dx@gmail.com>
2023-02-10 22:56:43 +0530

    navlink style update

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,15 +1,18 @@
 import React from "react";
 // import { NavLink } from "react-router-dom";
 import { Navlink } from "./NavbarStyles";
-import { Navbar } from "./NavbarStyles";
+import { NavContainer, Navbar, NavHeading } from "./NavbarStyles";
 
 const NavBar = () => {
   return (
-    <Navbar>
-      <Navlink to="/">Home</Navlink>
-      <Navlink to="about">About</Navlink>
-      <Navlink to="contact">Contact</Navlink>
-    </Navbar>
+    <NavContainer>
+      <NavHeading>Navbar</NavHeading>
+      <Navbar>
+        <Navlink to="/">Home</Navlink>
+        <Navlink to="about">About</Navlink>
+        <Navlink to="contact">Contact</Navlink>
+      </Navbar>
+    </NavContainer>
   );
 };
 
--- src/components/Navbar/NavbarStyles.js
+++ src/components/Navbar/NavbarStyles.js
@@ -1,14 +1,25 @@
 import styled from "styled-components";
 import { NavLink } from "react-router-dom";
 
-export const Navbar = styled.div`
+export const NavContainer = styled.div`
   background: gray;
+  display: flex;
+  justify-content: space-between;
+`;
+
+export const Navbar = styled.div`
   height: 10vh;
   display: flex;
   justify-content: end;
   align-items: center;
 `;
+
+export const NavHeading = styled.h1`
+  display: flex;
+`;
+
 export const Navlink = styled(NavLink)`
   padding-right: 1rem;
   text-decoration: none;
+  color: aquamarine;
 `;
