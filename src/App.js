5644008a00cb572e649a54b5ca331db38cb6a9e1
Abhinash Bhengra <abhinashbhengra170dx@gmail.com>
2023-02-10 21:53:54 +0530

    nav routing update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+
+// Pages
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
 
 function App() {
   return (
     <div className="App">
-      <h1>hello test</h1>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="contact" element={<Contact />} />
+      </Routes>
     </div>
   );
 }
